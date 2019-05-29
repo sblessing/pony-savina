25d25cc87ed7aaca0236218ee58b77533f27b965
Sebastian Blessing <sebastian.blessing@abs-gmbh.de>
2019-05-29 18:12:05 +0200

    fixed akka runner

--- runners/akka.py
+++ runners/akka.py
@@ -8,11 +8,11 @@ def setup(oBenchmarkRunner, cores):
   for root, dirs, files in os.walk("savina-jvm/target/classes/edu/rice/habanero/benchmarks/"):
     for file in files:
       if not pattern.fullmatch(file) is None:
-        classfiles.append(os.path.join(root, os.path.splitext(file)[0]))
+        classfiles.append(os.path.join(root, os.path.splitext(file)[0]).replace("savina-jvm/target/classes/", ""))
 
   nested_args = []
 
   for classfile in classfiles:
-    nested_args.append([["-cp", "savina-jvm/savina-0.0.1-SNAPSHOT-jar-with-dependencies.jar"], classfile])
+    nested_args.append([["-cp", "savina-jvm/target/savina-0.0.1-SNAPSHOT-jar-with-dependencies.jar"], classfile])
 
   oBenchmarkRunner.configure("akka", "/usr/bin/java", nested_args)
