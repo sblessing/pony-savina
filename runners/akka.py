b307d50176c381abdd506c432454d03e940b677f
Sebastian Blessing <sebastian.blessing@abs-gmbh.de>
2019-05-29 15:21:35 +0200

    akka runner regex fix

--- runners/akka.py
+++ runners/akka.py
@@ -6,7 +6,7 @@ def setup(oBenchmarkRunner, cores):
 
   for root, dirs, files in os.walk("savina-jvm/target/classes/edu/rice/habanero/benchmarks/"):
     for file in files:
-        if re.match("*Akka*Benchmark*.class", file):
+        if re.match(".*Akka.*Benchmark.*\.class", file):
              classfiles.append(os.path.join(root, os.path.splitext(file)[0]))
 
   nested_args = []
