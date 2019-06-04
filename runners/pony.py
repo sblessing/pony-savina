c96e1279ce9ae356d0c17792e4c63fb6ba7f4c10
Sebastian Blessing <sebastian.blessing@abs-gmbh.de>
2019-06-04 19:20:45 +0200

    fixed pony runner

--- runners/pony.py
+++ runners/pony.py
@@ -1,2 +1,2 @@
 def setup(oBenchmarkRunner, cores):
-  oBenchmarkRunner.configure("pony", "savina-pony/build/bin/", ["--parseable --ponythreads " + str(cores)])
+  oBenchmarkRunner.configure("pony", "savina-pony/build/bin/", ["--parseable", "--ponythreads", str(cores)])
